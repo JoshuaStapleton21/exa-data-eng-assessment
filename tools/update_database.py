9737eb2a2fecbacef8c000cf4d8d1f59870e35fe
Joshua-Stapleton <joshua.stapleton.ai@gmail.com>
2023-03-22 10:37:22 +0200

    added functions to tooling for creating 2NF database tables

--- tools/update_database.py
+++ tools/update_database.py
@@ -8,18 +8,33 @@ for k,v in list(sys.modules.items()):
         importlib.reload(v)
 
 
-def create_tables(patient_database_conn):
+def create_table(SQL_CREATE_COMMAND:str, patient_database_conn) -> bool:
     '''
     Creates the tables in the patient database
     :param patient_database_conn: the connection to the patient database
-    :return: None
+    :return: True if the operation succeeded
     '''
-    patient_table_fields = misc.get_patient_field_list()
-
     patient_database_cusor = patient_database_conn.cursor()
-    patient_database_cusor.execute("CREATE TABLE PATIENT")
-    
-    patient_database_conn.close()
+    try:
+        patient_database_cusor.execute(SQL_CREATE_COMMAND)
+        return True
+    except:
+        print("Error: Could not create patient table.")
+        return False
 
 
+def write_patient_data(SQL_INSERT_COMMAND:str, patient_database_conn) -> bool:
+    '''
+    Writes the patient data into the patient table
+    :param patient_database_conn: the connection to the patient database
+    :param patient_data: the patient data
+    :return: True if the operation succeeded
+    '''
+    patient_database_cusor = patient_database_conn.cursor()
+    try:
+        patient_database_cusor.execute(SQL_INSERT_COMMAND)
+        return True
+    except:
+        print("Error: Could not create patient table.")
+        return False
 
