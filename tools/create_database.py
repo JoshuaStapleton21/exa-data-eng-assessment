7f0fc62f9631095a6c760ffa8b7d16706243748d
Joshua-Stapleton <joshua.stapleton.ai@gmail.com>
2023-03-22 15:40:52 +0200

    replaced mysql functionality with sqlalchemy, re-wrote and simplified functions to write and read from SQL database

--- tools/create_database.py
+++ tools/create_database.py
@@ -1,61 +1,25 @@
 import os
 import sys
 sys.path.insert(1, '..')
-import mysql.connector
+from sqlalchemy import create_engine
 import importlib
 for k,v in list(sys.modules.items()):
     if k.startswith('tools'):
         importlib.reload(v)
+
 
 # use the dotenv framework to securely store credentials as environment variables
 from dotenv import load_dotenv
 load_dotenv()
 HOST = os.environ.get('host')
 USER = os.environ.get('user')
-PASSWORD = os.environ.get('password')
-DATABASE = os.environ.get('database')
+ENGINE = create_engine("{}://{}:".format(HOST, USER), echo=False) # change to true for verbose output
 
 
-def connect_to_mysql_server():
+# create and connect to a a basic in-memory sqla database using the login credentials
+def connect_to_sqla_server():
     """
-    Connects to a MySQL server
-    :return: a mysql connection object
+    Creates and connects to a SQLAlchemy server
+    :return: an sqla engine connection
     """
-    connection = mysql.connector.connect(
-        host=HOST,
-        user=USER,
-        password=PASSWORD
-    )
-    return connection
-
-
-def connect_to_patient_database(): 
-    """
-    Connects to the MySQL patient database
-    :return: a mysql connection object
-    """
-    try:
-        connection = mysql.connector.connect(
-            host=HOST,
-            user=USER,
-            password=PASSWORD,
-            database=DATABASE
-        )   
-        return connection
-    except:
-        print("Error: Could not connect to database. The connection strings are invalid")
-        return False
-
-
-def create_patient_database():
-    """
-    Creates a MySQL patient database if it does not exist.
-    :return: a mysql connection object for the patient database
-    """
-    mysql_server_connection = connect_to_mysql_server()
-    mysql_server_cursor = mysql_server_connection.cursor()
-    mysql_server_cursor.execute("CREATE DATABASE IF NOT EXISTS {}".format(DATABASE))
-    mysql_server_connection.commit()
-    mysql_server_connection.close()
-
-    return connect_to_patient_database()
+    return ENGINE.connect()
--- tools/update_database.py
+++ tools/update_database.py
@@ -1,40 +1,21 @@
-import os
 import sys
 sys.path.insert(1, '..')
-import tools.misc as misc
+from sqlalchemy import text
 import importlib
 for k,v in list(sys.modules.items()):
     if k.startswith('tools'):
         importlib.reload(v)
 
 
-def create_table(SQL_CREATE_COMMAND:str, patient_database_conn) -> bool:
+def execute_sql(SQL_CREATE_COMMAND:str, patient_database_conn) -> bool:
     '''
-    Creates the tables in the patient database
+    Executes commands in the SQL database
     :param patient_database_conn: the connection to the patient database
     :return: True if the operation succeeded
     '''
-    patient_database_cusor = patient_database_conn.cursor()
     try:
-        patient_database_cusor.execute(SQL_CREATE_COMMAND)
+        patient_database_conn.execute(text(SQL_CREATE_COMMAND))
         return True
-    except:
-        print("Error: Could not create patient table.")
+    except Exception as e:
+        print("Error: Could not execute command. {}".format(e))
         return False
-
-
-def write_patient_data(SQL_INSERT_COMMAND:str, patient_database_conn) -> bool:
-    '''
-    Writes the patient data into the patient table
-    :param patient_database_conn: the connection to the patient database
-    :param patient_data: the patient data
-    :return: True if the operation succeeded
-    '''
-    patient_database_cusor = patient_database_conn.cursor()
-    try:
-        patient_database_cusor.execute(SQL_INSERT_COMMAND)
-        return True
-    except:
-        print("Error: Could not create patient table.")
-        return False
-
